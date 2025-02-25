875a77a44b00babbee215136812faa5ebf0cfd50
nile <nile.black@gmail.com>
2025-02-25 16:01:54 +0800

    Configure TeaVM build and add HTML template for web deployment

--- teavm/src/main/java/code123/games/crystal/teavm/TeaVMLauncher.java
+++ teavm/src/main/java/code123/games/crystal/teavm/TeaVMLauncher.java
@@ -11,11 +11,11 @@ public class TeaVMLauncher {
     public static void main(String[] args) {
         TeaApplicationConfiguration config = new TeaApplicationConfiguration("canvas");
         //// If width and height are each greater than 0, then the app will use a fixed size.
-        // config.width = 800;
-        // config.height = 600;
+        config.width = 800;
+        config.height = 600;
         //// If width and height are both 0, then the app will use all available space.
-        config.width = 0;
-        config.height = 0;
+        // config.width = 0;
+        // config.height = 0;
         //// If width and height are both -1, then the app will fill the canvas size.
         // config.width = -1;
         // config.height = -1;
